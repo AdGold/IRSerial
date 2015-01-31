c82dfe62dcd987e078b74681dbebf6940bb3ad72
Adrian Goldwaser <agold@hotmail.com>
2015-01-31 22:47:40 +1100

    switched way of doing commands and added cmds for new remote

--- IRserial.py
+++ IRserial.py
@@ -1,54 +1,60 @@
 #!/usr/bin/python
 
-import serial,os
-import media
+import serial
+import os
+from media import send as media
 
-cmd = os.system
-for i in range(3):
-    try: ser = serial.Serial('/dev/ttyUSB%d'%i, 115200)
-    except Exception as e:
-        if i == 2: raise e
-    else: break
-print('Connected to', ser.name)
-MODEL = 0
-
-CMDS = {
-168 : ('RANDOM', 'awsetbg -r /home/adrian/Wallpaper/DesktopPhotos'),
-112 : ('SLEEP', 'sudo pm-suspend')
-}
 
 def restart():
     cmd('python ~/scripts/IRSerial/IRserial.py')
     exit()
 
-FUNCTIONS = {
-176 : ('X-BASS - restart sccript',  restart),
-0   : ('POWER - exit',              exit),
-32  : ('CD - Play/Pause',           lambda:media.send('PlayPause')),
-40  : ('USB - Play/Pause',          lambda:media.send('PlayPause')),
-144 : ('<< - Previous track',       lambda:media.send('Prev')),
-192 : ('>> - Next track',           lambda:media.send('Next')),
-16  : ('STOP - Stop',               lambda:media.send('Stop')),
-72  : ('REPEAT - toggle repeat',    lambda:media.send('Repeat')),
-80 : ('VOL DOWN',                   lambda:media.send('down', 'beep')),
-208 : ('VOL UP',                    lambda:media.send('up', 'beep')),
+COMMANDS = {
+    0: {
+        203: ('Restart script',   restart),
+        0: ('Quit',               exit),
+        209: ('Play/Pause',       lambda: media('PlayPause')),
+        169: ('Previous track',   lambda: media('Prev')),
+        83: ('Next track',        lambda: media('Next')),
+        147: ('Stop',             lambda: media('Stop')),
+        41: ('Repeat',            lambda: media('Repeat')),
+        11: ('Vol Down',          lambda: media('down', 'beep')),
+        139: ('Vol Up',           lambda: media('up', 'beep')),
+        105: ('Mute',             lambda: media('mute')),
+        163: ('Slepp',            'sudo pm-suspend'),
+        75: ('Right',             'i3-msg workspace next'),
+        9: ('Left',               'i3-msg workspace prev'),
+    },
+    6: {
+        126: ('Vol Down',          lambda: media('down')),
+        190: ('Vol Up',           lambda: media('up')),
+    },
 }
 
+run = os.system
+for i in range(3):
+    try:
+        ser = serial.Serial('/dev/ttyUSB%d' % i, 115200)
+    except Exception as e:
+        if i == 2:
+            raise e
+    else:
+        break
+print('Connected to', ser.name)
+
 while 1:
-  try:
-    inp = ser.readline().strip()
-    m,v = list(map(int,inp.split()))
-  except Exception:
-    print(inp)
-  else:
-    if m == MODEL:
-      if v in CMDS:
-        cmd(CMDS[v][1])
-        print(CMDS[v][0])
-      elif v in FUNCTIONS:
-        FUNCTIONS[v][1]()
-        print(FUNCTIONS[v][0])
-      else:
-        print(v)
-
-
+    try:
+        inp = ser.readline().strip()
+        m, v = list(map(int, inp.split()))
+    except Exception:
+        print(inp)
+    else:
+        if m in COMMANDS and v in COMMANDS[m]:
+            cmd = COMMANDS[m][v]
+            if isinstance(cmd[1], str):
+                run(cmd[1])
+            else:
+                cmd[1]()
+            print(cmd[0])
+        else:
+            print('%d %d' % (m, v))
