af3c5c23b67b054bfd7aca95b1f6dc1a46109647
Adrian Goldwaser <agold@hotmail.com>
2016-03-11 02:00:28 +1100

    Fix general style and volume for media.py

--- media.py
+++ media.py
@@ -1,58 +1,72 @@
 #! /usr/bin/python
 
-import os, subprocess, sys, dbus, re
+import os
+import subprocess
+import sys
+import re
 from clementine import Clementine
 
-vlc_pre = 'dbus-send --session --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.'
+vlc_pre = 'dbus-send --session --type=method_call --print-reply'\
+    '--dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 '\
+    'org.mpris.MediaPlayer2.Player.'
 
 vlc = {
-    'playpause':'PlayPause',
-    'next':'Next',
-    'prev':'Previous',
-    'stop':'Stop',
-    'repeat':'Repeat',
+    'playpause': 'PlayPause',
+    'next': 'Next',
+    'prev': 'Previous',
+    'stop': 'Stop',
+    'repeat': 'Repeat',
 }
 
 mplayer_fifo = '/home/adrian/.mplayerremote'
 mplayer_format = 'echo "{}" > '+mplayer_fifo
 
 mplayer = {
-    'playpause':'pause', #doesn't work on a complete stop...
-    'next':'pt_step 1 1', #only steps while playing video
-    'prev':'pt_step -1 1', #also moves forward...
-    'stop':'stop', #only part that works properly
-    'repeat':'Repeat', #actually loop 0 for repeat and loop -1 
+    'playpause': 'pause',  # doesn't work on a complete stop...
+    'next': 'pt_step 1 1',  # only steps while playing video
+    'prev': 'pt_step -1 1',  # also moves forward...
+    'stop': 'stop',  # only part that works properly
+    'repeat': 'Repeat',  # actually loop 0 for repeat and loop -1
 }
 
 vol_step = 2
 vol_timeout = 500
 vol_beep = '/home/adrian/scripts/IRSerial/volume.wav'
 volume_cmds = {
-    'up':'amixer -D pulse sset Master {}%+'.format(vol_step),
-    'down':'amixer -D pulse sset Master {}%-'.format(vol_step),
-    'mute':'amixer -D pulse sset Master toggle'
+    'up': 'amixer -D pulse sset Master {}%+'.format(vol_step),
+    'down': 'amixer -D pulse sset Master {}%-'.format(vol_step),
+    'mute': 'amixer -D pulse sset Master toggle'
 }
 
 if not os.path.exists(mplayer_fifo):
     os.system('mkfifo '+mplayer_fifo)
 
+
 def do_volume(cmd, snd):
     os.system(volume_cmds[cmd])
-    detail = subprocess.check_output(['amixer', '-D', 'pulse', 'get', 'Master']).decode('utf-8')
+    cmd = ['amixer', '-D', 'pulse', 'get', 'Master']
+    detail = subprocess.check_output(cmd).decode('utf-8')
     detail = [line for line in detail.split('\n') if '%]' in line][0]
     muted = 'off' in detail
     if not muted and snd:
-        #os.system('mplayer %s' % vol_beep)
+        # os.system('mplayer %s' % vol_beep)
         os.system('aplay %s' % vol_beep)
     volume = int(re.search(r'\[(\d+)%\]', detail).group(1))
-    if cmd == 'mute' and muted:
+    if muted:
         icon = 'audio-volume-muted'
+    elif volume == 0:
+        icon = 'audio-volume-off'
     else:
         icon = 'audio-volume-' + ('low', 'medium', 'high')[volume//34]
-    blocks = volume//5
-    bar = '[' + '='*blocks + '-'*(2*(20-blocks)) + ']'
-    title = 'Volume: ' + (str(volume)+'%' if not muted else 'Muted')
-    os.system('notify-send %r %r -i %r -t %d' % (title, bar, icon, vol_timeout))
+    # blocks = volume//5
+    # bar = '[' + '='*blocks + '-'*(2*(20-blocks)) + ']'
+    # title = 'Volume: ' + (str(volume)+'%' if not muted else 'Muted')
+    # os.system('notify-send %r %r -i %r -t %d' %
+    # (title, bar, icon, vol_timeout))
+    cmd = 'notify-send " " -i notification-%s -t %d ' \
+        '-h int:value:%d -h string:synchronous:volume'
+    os.system(cmd % (icon, vol_timeout, volume))
+
     return volume, muted
 
 
